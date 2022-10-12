ea36e74ff5536d369982312b5f9491768fe2ff2a
modamod <modhaffer.rahmani@gmail.com>
2022-10-12 14:38:36 +0100

    Mounted vendor folder on the docker container to improve speed

--- docker/entrypoint_alpine.sh
+++ docker/entrypoint_alpine.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+cd /var/www/html
 # fix key if needed
 if [ -z "$APP_KEY" ]
 then
@@ -39,6 +39,7 @@ chown -R apache:root /var/lib/snipeit/data/*
 chown -R apache:root /var/lib/snipeit/dumps
 chown -R apache:root /var/lib/snipeit/keys
 chown -R apache:root /var/www/html/storage/
+chown -R apache:apache /var/www/html
 
 # If the Oauth DB files are not present copy the vendor files over to the db migrations
 if [ ! -f "/var/www/html/database/migrations/*create_oauth*" ]
@@ -55,7 +56,30 @@ php artisan migrate --force
 php artisan config:clear
 php artisan config:cache
 
-chown -R apache:root /var/www/html/storage/logs/laravel.log
+
+
+
+
+
+rm -r "/var/www/html/storage/private_uploads" \
+&& mkdir -p "/var/lib/snipeit/data/private_uploads" && ln -fs "/var/lib/snipeit/data/private_uploads" "/var/www/html/storage/private_uploads" \
+  && rm -rf "/var/www/html/public/uploads" \
+  && mkdir -p "/var/lib/snipeit/data/uploads" && ln -fs "/var/lib/snipeit/data/uploads" "/var/www/html/public/uploads" \
+  && mkdir -p "/var/lib/snipeit/dumps" && rm -r "/var/www/html/storage/app/backups" && ln -fs "/var/lib/snipeit/dumps" "/var/www/html/storage/app/backups" \
+  && mkdir -p "/var/lib/snipeit/keys" && ln -fs "/var/lib/snipeit/keys/oauth-private.key" "/var/www/html/storage/oauth-private.key" \
+  && ln -fs "/var/lib/snipeit/keys/oauth-public.key" "/var/www/html/storage/oauth-public.key" \
+  && chown -hR apache "/var/www/html/storage/" \
+  && chown -R apache "/var/lib/snipeit"
+
+mkdir -p /var/www/.composer && chown apache /var/www/.composer
+
+
+if  [ "${APP_ENV}" = "develop" ]; \
+then COMPOSER_CACHE_DIR=/dev/null composer install  --working-dir=/var/www/html; \
+else COMPOSER_CACHE_DIR=/dev/null composer install  --no-dev --working-dir=/var/www/html; \
+fi
+
+chown -R apache:apache /var/www/html
 
 export APACHE_LOG_DIR=/var/log/apache2
 exec httpd -DNO_DETACH < /dev/null
